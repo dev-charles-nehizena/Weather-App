21da0077559856c736ef2feebeb4c2fad39b7b0b
Charles <charlessmart205@gmail.com>
2023-08-26 15:33:45 -0700

    done

---
+++ main.js
@@ -0,0 +1,37 @@
+let weather = {
+    "apiKeys": "5bde1c48e7e7612006abb211409fcc72",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q= " 
+            + city 
+            + " &unit=metric&appid=" 
+            + this.apiKeys
+        )
+        .then((respons => respons.json()))
+        .then((data => this.displayWeather(data)));
+    },
+
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector (".icon").src = 
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+            document.querySelector(".description").innerText = description;
+            document.querySelector(".temp").innerText = temp + "°C";
+            document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+            document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+
+    },
+    search: function(){
+        this,this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
